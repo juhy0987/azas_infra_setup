78885d56f99c12d783911a97e89c596deabb02af
parkyhun <yy9976com@gamil.com>
2026-05-08 14:09:23 +0900

    [REFAC]: ASG SNS 오토 스케일링 수정

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,19 +1,35 @@
 # 0. Launch Template
 resource "aws_launch_template" "project_lt" {
-  name_prefix          = "azas-launch-template"
+  name_prefix   = "azas-launch-template"
   image_id      = data.aws_ami.al2023.id
   instance_type = "t3.micro"
 
   network_interfaces {
     security_groups = [aws_security_group.ec2_sg.id]
   }
+
   user_data = base64encode(<<-EOF
-      #!/bin/bash
-      dnf install -y nginx
-      systemctl enable nginx
-      systemctl start nginx
-    EOF
+    #!/bin/bash
+
+    # 1. nginx 설치
+    dnf install -y nginx
+    systemctl enable nginx
+    systemctl start nginx
+
+    # 2. node_exporter 설치
+    cd /tmp
+    wget -q https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+    if [ $? -eq 0 ]; then
+      tar xvfz node_exporter-1.7.0.linux-amd64.tar.gz
+      cp node_exporter-1.7.0.linux-amd64/node_exporter /usr/local/bin/
+      printf '[Unit]\nDescription=Node Exporter\nAfter=network.target\n\n[Service]\nUser=root\nExecStart=/usr/local/bin/node_exporter\n\n[Install]\nWantedBy=multi-user.target\n' > /etc/systemd/system/node_exporter.service
+      systemctl daemon-reload
+      systemctl enable node_exporter
+      systemctl start node_exporter
+    fi
+  EOF
   )
+
   update_default_version = true
 
   lifecycle {
@@ -21,29 +37,14 @@ resource "aws_launch_template" "project_lt" {
   }
 }
 
-# 1. SNS 토픽 생성
-resource "aws_sns_topic" "asg_alert" {
-  name = "project-asg-alert-topic"
-}
-
-# 2. 이메일 구독
-resource "aws_sns_topic_subscription" "email_subs" {
-  topic_arn = aws_sns_topic.asg_alert.arn
-  protocol  = "email"
-  endpoint  = var.alert_email
-}
-
-# 3. 오토 스케일링 그룹 (ASG)
+# 1. 오토 스케일링 그룹
 resource "aws_autoscaling_group" "project_asg" {
   name_prefix      = "my-project-asg"
   desired_capacity = 1
   max_size         = 4
   min_size         = 1
 
-  vpc_zone_identifier = [
-    aws_subnet.private_subnet_a.id
-    #aws_subnet.private_subnet_c.id                    
-  ]
+  vpc_zone_identifier = [aws_subnet.private_subnet_a.id]
 
   launch_template {
     id      = aws_launch_template.project_lt.id
@@ -65,12 +66,11 @@ resource "aws_autoscaling_group" "project_asg" {
   }
 }
 
-# 4. ASG 스케일링 정책 
+# 2. ASG 스케일링 정책
 resource "aws_autoscaling_policy" "cpu_policy" {
   name                   = "cpu-70-percent-policy"
   autoscaling_group_name = aws_autoscaling_group.project_asg.name
-
-  policy_type = "TargetTrackingScaling"
+  policy_type            = "TargetTrackingScaling"
 
   target_tracking_configuration {
     predefined_metric_specification {
@@ -80,21 +80,12 @@ resource "aws_autoscaling_policy" "cpu_policy" {
   }
 }
 
-# 5. ASG → SNS 알림 연결
-resource "aws_autoscaling_notification" "asg_notify" {
-  group_names = [aws_autoscaling_group.project_asg.name]
-  topic_arn   = aws_sns_topic.asg_alert.arn
-
-  notifications = [
-    "autoscaling:EC2_INSTANCE_LAUNCH",
-    "autoscaling:EC2_INSTANCE_TERMINATE",
-    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
-    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
-  ]
-}
-
-# 6. 변수
-variable "alert_email" {
-  description = "ASG 알림 받을 이메일 주소"
-  type        = string
+# 3. 보안 그룹 규칙 추가
+resource "aws_security_group_rule" "allow_prometheus" {
+  type              = "ingress"
+  from_port         = 9100
+  to_port           = 9100
+  protocol          = "tcp"
+  security_group_id = aws_security_group.ec2_sg.id
+  cidr_blocks       = ["172.16.8.200/32"]
 }
