1953b32e07366b0a0f3bbc290d0f0ee1fd230bd3
juhyun <juhy0987@naver.com>
2026-05-10 23:04:11 +0900

    [FIX]: aws_launch_template에 key_name 속성 추가

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -4,6 +4,7 @@ resource "aws_launch_template" "project_lt" {
   name_prefix   = "azas-launch-template"
   image_id      = local.baked_ami_id
   instance_type = "t3.micro"
+  key_name      = aws_key_pair.kp.key_name
 
   network_interfaces {
     security_groups = [aws_security_group.ec2_sg.id]
